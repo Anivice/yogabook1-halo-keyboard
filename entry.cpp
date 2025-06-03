d43bdf10def7db1bb3cd4360e3a6a176a4610aa4
Anivice Ives <aniviceives@gmail.com>
2025-06-03 22:41:37 +0800

    Added long press and further decreased special cases, simplified logic

--- entry.cpp
+++ entry.cpp
@@ -19,15 +19,17 @@
 #include <sys/ioctl.h>
 #include <key_id.h>
 
-enum KeyAction { KEY_COMBINATION_ONLY };
+constexpr unsigned int long_press_interval_ms = 80;
+
+enum KeyAction { KEY_COMBINATION_ONLY, FN_KEY_INVERT_MAP };
 
 // these keys are locked keys, locked keys are not released until one non-lockable key press
 // when forming a combination with normal keys, long press mode is disabled
 const std::map < const unsigned int, const KeyAction > SpecialKeys =
 {
-    // { 464   /* Fn */,       KEY_COMBINATION_ONLY },
+    // { 464   /* Fn */,       FN_KEY_INVERT_MAP }, TODO: Fn Key
     { 29    /* LCtrl */,    KEY_COMBINATION_ONLY },
-    //{ 125   /* Win */,      KEY_COMBINATION_ONLY },
+    // { 125   /* Win */,      KEY_COMBINATION_AND_NORMAL_PRESS },
     { 56    /* LAlt */,     KEY_COMBINATION_ONLY },
     { 100   /* RAlt */,     KEY_COMBINATION_ONLY },
     { 97    /* RCtrl */,    KEY_COMBINATION_ONLY },
@@ -36,165 +38,32 @@ const std::map < const unsigned int, const KeyAction > SpecialKeys =
 };
 
 const std::vector < unsigned int > keys_supporting_long_press = {
-    KEY_ID_SPACE,
-    KEY_ID_PGUP,
-    KEY_ID_UP,
-    KEY_ID_PGDN,
-    KEY_ID_LEFT,
-    KEY_ID_DOWN,
-    KEY_ID_RIGHT,
-    KEY_ID_Z,
-    KEY_ID_X,
-    KEY_ID_C,
-    KEY_ID_V,
-    KEY_ID_B,
-    KEY_ID_N,
-    KEY_ID_M,
-    KEY_ID_LESS,
-    KEY_ID_LARGER,
-    KEY_ID_QUESTION,
-    KEY_ID_A,
-    KEY_ID_S,
-    KEY_ID_D,
-    KEY_ID_F,
-    KEY_ID_G,
-    KEY_ID_H,
-    KEY_ID_J,
-    KEY_ID_K,
-    KEY_ID_L,
-    KEY_ID_SEMICOLON,
-    KEY_ID_DOUBLEQUOTE,
-    KEY_ID_ENTER,
-    KEY_ID_TAB,
-    KEY_ID_Q,
-    KEY_ID_W,
-    KEY_ID_E,
-    KEY_ID_R,
-    KEY_ID_T,
-    KEY_ID_Y,
-    KEY_ID_U,
-    KEY_ID_I,
-    KEY_ID_O,
-    KEY_ID_P,
-    KEY_ID_LEFTBRACE,
-    KEY_ID_RIGHTBRACE,
-    KEY_ID_BACKSLASH,
-    KEY_ID_GRAVE,
-    KEY_ID_1,
-    KEY_ID_2,
-    KEY_ID_3,
-    KEY_ID_4,
-    KEY_ID_5,
-    KEY_ID_6,
-    KEY_ID_7,
-    KEY_ID_8,
-    KEY_ID_9,
-    KEY_ID_0,
-    KEY_ID_MINUS,
-    KEY_ID_EQUAL,
-    KEY_ID_BACKSPACE,
-    KEY_ID_DELETE,
+    KEY_ID_SPACE, KEY_ID_PGUP, KEY_ID_UP, KEY_ID_PGDN, KEY_ID_LEFT, KEY_ID_DOWN, KEY_ID_RIGHT,
+    KEY_ID_Z, KEY_ID_X, KEY_ID_C, KEY_ID_V, KEY_ID_B, KEY_ID_N, KEY_ID_M,
+    KEY_ID_LESS, KEY_ID_LARGER, KEY_ID_QUESTION,
+    KEY_ID_A, KEY_ID_S, KEY_ID_D, KEY_ID_F, KEY_ID_G, KEY_ID_H, KEY_ID_J, KEY_ID_K, KEY_ID_L,
+    KEY_ID_SEMICOLON, KEY_ID_DOUBLEQUOTE, KEY_ID_ENTER, KEY_ID_TAB,
+    KEY_ID_Q, KEY_ID_W, KEY_ID_E, KEY_ID_R, KEY_ID_T, KEY_ID_Y, KEY_ID_U, KEY_ID_I, KEY_ID_O, KEY_ID_P,
+    KEY_ID_LEFTBRACE, KEY_ID_RIGHTBRACE, KEY_ID_BACKSLASH, KEY_ID_GRAVE,
+    KEY_ID_1, KEY_ID_2, KEY_ID_3, KEY_ID_4, KEY_ID_5, KEY_ID_6, KEY_ID_7, KEY_ID_8, KEY_ID_9, KEY_ID_0,
+    KEY_ID_MINUS, KEY_ID_EQUAL, KEY_ID_BACKSPACE, KEY_ID_DELETE,
 };
 
-const std::map < const unsigned int, const std::string > key_id_to_str_translation_table =
-{
-    // group 1
-    { KEY_ID_FN, "Fn" },
-    { KEY_ID_LCTRL, "LCtrl" },
-    { KEY_ID_WIN, "Win" },
-    { KEY_ID_LALT, "LAlt" },
-    { KEY_ID_SPACE, "Space" },
-    { KEY_ID_RALT, "RAlt" },
-    { KEY_ID_RCTRL, "RCtrl" },
-    { KEY_ID_PGUP, "PgUp" },
-    { KEY_ID_UP, "Up" },
-    { KEY_ID_PGDN, "PgDn" },
-    // + 3
-    { KEY_ID_LEFT, "Left" },
-    { KEY_ID_DOWN, "Down" },
-    { KEY_ID_RIGHT, "Right" },
-
-    // group 2
-    { KEY_ID_LSHIFT, "LShift" },
-    { KEY_ID_Z, "Z" },
-    { KEY_ID_X, "X" },
-    { KEY_ID_C, "C" },
-    { KEY_ID_V, "V" },
-    { KEY_ID_B, "B" },
-    { KEY_ID_N, "N" },
-    { KEY_ID_M, "M" },
-    { KEY_ID_LESS, "<," },
-    { KEY_ID_LARGER, ">." },
-    { KEY_ID_QUESTION, "?/" },
-    { KEY_ID_RSHIFT, "RShift" },
-
-    // group 3
-    { KEY_ID_CAPSLOCK, "CapsLock" },
-    { KEY_ID_A, "A" },
-    { KEY_ID_S, "S" },
-    { KEY_ID_D, "D" },
-    { KEY_ID_F, "F" },
-    { KEY_ID_G, "G" },
-    { KEY_ID_H, "H" },
-    { KEY_ID_J, "J" },
-    { KEY_ID_K, "K" },
-    { KEY_ID_L, "L" },
-    { KEY_ID_SEMICOLON, ":;" },
-    { KEY_ID_DOUBLEQUOTE, "\"\'" },
-    { KEY_ID_ENTER, "Enter" },
-
-    // group 4
-    { KEY_ID_TAB, "Tab" },
-    { KEY_ID_Q, "Q" },
-    { KEY_ID_W, "W" },
-    { KEY_ID_E, "E" },
-    { KEY_ID_R, "R" },
-    { KEY_ID_T, "T" },
-    { KEY_ID_Y, "Y" },
-    { KEY_ID_U, "U" },
-    { KEY_ID_I, "I" },
-    { KEY_ID_O, "O" },
-    { KEY_ID_P, "P" },
-    { KEY_ID_LEFTBRACE, "{[" },
-    { KEY_ID_RIGHTBRACE, "}]" },
-    { KEY_ID_BACKSLASH, "|\\" },
-
-    // group 5
-    { KEY_ID_GRAVE, "~`" },
-    { KEY_ID_1, "1" },
-    { KEY_ID_2, "2" },
-    { KEY_ID_3, "3" },
-    { KEY_ID_4, "4" },
-    { KEY_ID_5, "5" },
-    { KEY_ID_6, "6" },
-    { KEY_ID_7, "7" },
-    { KEY_ID_8, "8" },
-    { KEY_ID_9, "9" },
-    { KEY_ID_0, "0" },
-    { KEY_ID_MINUS, "_-" },
-    { KEY_ID_EQUAL, "+=" },
-    { KEY_ID_BACKSPACE, "BackSpace" },
-
-    // group 6
-    { KEY_ID_ESC, "ESC" },
-    { KEY_ID_F1, "F1" },
-    { KEY_ID_F2, "F2" },
-    { KEY_ID_F3, "F3" },
-    { KEY_ID_F4, "F4" },
-    { KEY_ID_F5, "F5" },
-    { KEY_ID_F6, "F6" },
-    { KEY_ID_F7, "F7" },
-    { KEY_ID_F8, "F8" },
-    { KEY_ID_F9, "F9" },
-    { KEY_ID_F10, "F10" },
-    { KEY_ID_F11, "F11" },
-    { KEY_ID_F12, "F12" },
-    { KEY_ID_DELETE, "Delete" },
-
-    // group 7
-    { KEY_ID_MOUSELEFT, "MouseLeft" },
-    { KEY_ID_MOUSERIGHT, "MouseRight" },
-    { KEY_ID_TOUCHPAD, "TouchPad" },
+// FIXME: This invert map might be inaccurate and should be specific
+const std::map < key_t, key_t > fn_key_invert_map = {
+    { KEY_ID_ESC, KEY_FN_ESC },
+         { KEY_ID_F1, KEY_FN_F1 },
+         { KEY_ID_F2, KEY_FN_F2 },
+         { KEY_ID_F3, KEY_FN_F3 },
+         { KEY_ID_F4, KEY_FN_F4 },
+         { KEY_ID_F5, KEY_FN_F5 },
+         { KEY_ID_F6, KEY_FN_F6 },
+         { KEY_ID_F7, KEY_FN_F7 },
+         { KEY_ID_F8, KEY_FN_F8 },
+         { KEY_ID_F9, KEY_FN_F9 },
+         { KEY_ID_F10, KEY_FN_F10 },
+         { KEY_ID_F11, KEY_FN_F11 },
+         { KEY_ID_F12, KEY_FN_F12 },
 };
 
 volatile std::atomic_int ctrl_c = 0;
@@ -334,7 +203,12 @@ void emit_key_thread()
                     /////////////////
                     else if (state.press_down)
                     {
-                        press_keys_once(key_id);
+                        if (key_id == KEY_ID_WIN) {
+                            std::lock_guard local_guard(mutex_);
+                            combination_sp_keys.push_back(KEY_ID_WIN);
+                        } else {
+                            press_keys_once(key_id);
+                        }
                         state.normal_press_handled = true;
                     }
                     ///////////////////
@@ -357,8 +231,12 @@ void emit_key_thread()
                                 [&](const long_press_struct & ins_state)->bool { return ins_state.key == key_id; });
                         }
 
+                        if (key_id == KEY_ID_WIN) {
+                            press_keys_once(KEY_ID_WIN); // now we press win
+                        }
+
                         std::lock_guard<std::mutex> lock(mutex_);
-                        std::erase(combination_sp_keys, key_id);
+                        std::erase(combination_sp_keys, key_id); // this will delete WIN as well
                         invalid_keys.push_back(key_id);
                         state.normal_press_handled = true;
                         if (DEBUG) debug::log("Key ", key_id_translate(key_id), " released\n");
@@ -369,7 +247,7 @@ void emit_key_thread()
                 /// LONG PRESS PROCESSING REGION ///
                 ////////////////////////////////////
                 if (const auto time_now = std::chrono::high_resolution_clock::now();
-                    // is initial key press already handled and still being pressed down?
+                    // is the initial key press already handled and still being pressed down?
                     state.normal_press_handled && state.press_down
                     // does this key actually support long press?
                     && std::ranges::find(keys_supporting_long_press, key_id) != keys_supporting_long_press.end()
@@ -386,7 +264,7 @@ void emit_key_thread()
                         while (*should_i_be_running)
                         {
                             press_keys_once(key_id);
-                            std::this_thread::sleep_for(std::chrono::milliseconds(200));
+                            std::this_thread::sleep_for(std::chrono::milliseconds(long_press_interval_ms));
                         }
                     };
 
--- include/key_id.h
+++ include/key_id.h
@@ -98,4 +98,107 @@ constexpr unsigned int KEY_ID_MOUSELEFT  = 272;
 constexpr unsigned int KEY_ID_MOUSERIGHT = 273;
 constexpr unsigned int KEY_ID_TOUCHPAD   = 512;
 
+#include <map>
+
+const std::map < const unsigned int, const std::string > key_id_to_str_translation_table =
+{
+    // group 1
+    { KEY_ID_FN, "Fn" },
+    { KEY_ID_LCTRL, "LCtrl" },
+    { KEY_ID_WIN, "Win" },
+    { KEY_ID_LALT, "LAlt" },
+    { KEY_ID_SPACE, "Space" },
+    { KEY_ID_RALT, "RAlt" },
+    { KEY_ID_RCTRL, "RCtrl" },
+    { KEY_ID_PGUP, "PgUp" },
+    { KEY_ID_UP, "Up" },
+    { KEY_ID_PGDN, "PgDn" },
+    // + 3
+    { KEY_ID_LEFT, "Left" },
+    { KEY_ID_DOWN, "Down" },
+    { KEY_ID_RIGHT, "Right" },
+
+    // group 2
+    { KEY_ID_LSHIFT, "LShift" },
+    { KEY_ID_Z, "Z" },
+    { KEY_ID_X, "X" },
+    { KEY_ID_C, "C" },
+    { KEY_ID_V, "V" },
+    { KEY_ID_B, "B" },
+    { KEY_ID_N, "N" },
+    { KEY_ID_M, "M" },
+    { KEY_ID_LESS, "<," },
+    { KEY_ID_LARGER, ">." },
+    { KEY_ID_QUESTION, "?/" },
+    { KEY_ID_RSHIFT, "RShift" },
+
+    // group 3
+    { KEY_ID_CAPSLOCK, "CapsLock" },
+    { KEY_ID_A, "A" },
+    { KEY_ID_S, "S" },
+    { KEY_ID_D, "D" },
+    { KEY_ID_F, "F" },
+    { KEY_ID_G, "G" },
+    { KEY_ID_H, "H" },
+    { KEY_ID_J, "J" },
+    { KEY_ID_K, "K" },
+    { KEY_ID_L, "L" },
+    { KEY_ID_SEMICOLON, ":;" },
+    { KEY_ID_DOUBLEQUOTE, "\"\'" },
+    { KEY_ID_ENTER, "Enter" },
+
+    // group 4
+    { KEY_ID_TAB, "Tab" },
+    { KEY_ID_Q, "Q" },
+    { KEY_ID_W, "W" },
+    { KEY_ID_E, "E" },
+    { KEY_ID_R, "R" },
+    { KEY_ID_T, "T" },
+    { KEY_ID_Y, "Y" },
+    { KEY_ID_U, "U" },
+    { KEY_ID_I, "I" },
+    { KEY_ID_O, "O" },
+    { KEY_ID_P, "P" },
+    { KEY_ID_LEFTBRACE, "{[" },
+    { KEY_ID_RIGHTBRACE, "}]" },
+    { KEY_ID_BACKSLASH, "|\\" },
+
+    // group 5
+    { KEY_ID_GRAVE, "~`" },
+    { KEY_ID_1, "1" },
+    { KEY_ID_2, "2" },
+    { KEY_ID_3, "3" },
+    { KEY_ID_4, "4" },
+    { KEY_ID_5, "5" },
+    { KEY_ID_6, "6" },
+    { KEY_ID_7, "7" },
+    { KEY_ID_8, "8" },
+    { KEY_ID_9, "9" },
+    { KEY_ID_0, "0" },
+    { KEY_ID_MINUS, "_-" },
+    { KEY_ID_EQUAL, "+=" },
+    { KEY_ID_BACKSPACE, "BackSpace" },
+
+    // group 6
+    { KEY_ID_ESC, "ESC" },
+    { KEY_ID_F1, "F1" },
+    { KEY_ID_F2, "F2" },
+    { KEY_ID_F3, "F3" },
+    { KEY_ID_F4, "F4" },
+    { KEY_ID_F5, "F5" },
+    { KEY_ID_F6, "F6" },
+    { KEY_ID_F7, "F7" },
+    { KEY_ID_F8, "F8" },
+    { KEY_ID_F9, "F9" },
+    { KEY_ID_F10, "F10" },
+    { KEY_ID_F11, "F11" },
+    { KEY_ID_F12, "F12" },
+    { KEY_ID_DELETE, "Delete" },
+
+    // group 7
+    { KEY_ID_MOUSELEFT, "MouseLeft" },
+    { KEY_ID_MOUSERIGHT, "MouseRight" },
+    { KEY_ID_TOUCHPAD, "TouchPad" },
+};
+
 #endif //KEY_ID_H_
